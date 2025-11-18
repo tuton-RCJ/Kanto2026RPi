a71c3a7f7d24e8e8f95b01c6358577478f32b492
shuji <shuji.igarashi@gmail.com>
2025-11-18 15:33:03 +0900

    completed communication(RPi)

--- src/device/deviceEnums.py
+++ src/device/deviceEnums.py
@@ -1,13 +1,28 @@
-from enum import Enum,auto
+from enum import Enum
 
-def Side(Enum):
+class Side(Enum):
     LEFT = 0
     RIGHT = 1
     
 
 
-def UnitVStatus(Enum):
+class UnitVStatus(Enum):
     UNKNOWN=0
     NOTHING=1
     
     
+class ActuatorControlType(Enum):
+    STS_MOTOR=bytes([1])
+    RESCUE_KIT=bytes([2])
+    LED=bytes([3])
+    BUZZER=bytes([4])
+    
+    def dataLength(self):
+        if self==ActuatorControlType.STS_MOTOR:
+            return 2
+        elif self==ActuatorControlType.RESCUE_KIT:
+            return 2
+        elif self==ActuatorControlType.LED:
+            return 3
+        elif self==ActuatorControlType.BUZZER:
+            return 1
--- src/device/stm.py
+++ src/device/stm.py
@@ -2,25 +2,103 @@ import serial
 import device.deviceEnums as deviceEnums
 import device.deviceConstrains as deviceConst
 from dataclasses import dataclass
+import time
 
 
 
-@dataclass
-class UARTData:
-    pass
 
 
 class STMUART:
-    def __init__(self, port="/dev/ttyAMA4"):
-        self.port = port
+    def __init__(self, port: str = "/dev/ttyAMA4",timeout: float = 0.5):
+        self._port = port
+        self._serial = serial.Serial(
+            port=self._port,
+            baudrate=115200,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1,
+        )
+        self._seq: int=0
+        self._timeout=timeout
 
-    def read(self) -> UARTData:
+    def read(self) -> bytes:
         # シリアル通信でSTMからデータを読む
-        return UARTData()
+        return self._serial.read(1)
+    
+    def write(self,data: bytes):
+        # シリアル通信でSTMにデータを書く
+        self._serial.write(data)
+        
+    def requestSensorValues(self)->bytes|None:
+        # センサの値を要求する
+        self._serial.write(b'\x00')
+        self.updateSeq()
+        self._serial.write(bytes([self._seq]))
+        checkDigit=0^self._seq
+        self._serial.write(bytes([checkDigit]))
+        
+        # レスポンス待機
+        start_time = time.time()
+        while(self._serial.in_waiting<11):
+            if time.time() - start_time > self._timeout:
+                print("STM UART timeout")
+                return None
+            
+        data:bytes= self._serial.read(11)
+        # データのチェック
+        if data[0]==0x00 and data[1]==self._seq:
+            checkDigit=0
+            for b in data[0:10]:
+                checkDigit ^= b
+            if checkDigit==data[10]:
+                return data[2:10]
+        print("STM UART data error")
+        return None
+    
+    def requestActuatorControl(self,type: deviceEnums.ActuatorControlType,data:bytes)->bool:
+         
+        # データ長のチェック
+        if len(data) != type.dataLength():
+            print("Actuator Control data length error")
+            return False
+        
+        self._serial.write(bytes(type.value))
+        self.updateSeq()
+        self._serial.write(bytes([self._seq]))
+        for b in data:
+            self._serial.write(bytes([b]))
+        checkDigit=0^self._seq
+        for b in data:
+            checkDigit ^= b
+        self._serial.write(bytes([checkDigit]))
+        
+        # レスポンス待機
+        start_time = time.time()
+        while(self._serial.in_waiting<3):
+            if time.time() - start_time > self._timeout:
+                print("STM UART timeout")
+                return False
+        
+        response:bytes= self._serial.read(3)
+        
+        # レスポンスのチェック
+        if response[0]==type.value[0] and response[1]==self._seq:
+            checkDigit=0^self._seq
+            if checkDigit==response[2]:
+                return True
+        print("STM UART response error")
+        return False
+        
+    def updateSeq(self):
+        self._seq+=1
+        self._seq=self._seq%256
+        
+        
 
 
-port = "/dev/ttyAMA4"
-stmUART = STMUART(port)
+port: str = "/dev/ttyAMA4"
+stmUART: STMUART = STMUART(port)
 
 
 class STS3032:
@@ -29,12 +107,25 @@ class STS3032:
     ):
         pass
 
-    def setMotorSpeed(self, motorSpeed: dict[deviceEnums.Side, int]):
+    def setMotorSpeed(self, motorSpeed: dict[deviceEnums.Side, int]) -> bool:
         """
         @brief モーターの速度を設定する
         @param motorSpeed: モーターの速度の辞書[Side, 速度(int,0~100)]
         """
-        pass
+        global stmUART
+        if not (0 <= motorSpeed[deviceEnums.Side.LEFT] <= 100):
+            print("invalid motor speed")
+            return False
+        if not (0 <= motorSpeed[deviceEnums.Side.RIGHT] <= 100):
+            print("invalid motor speed")
+            return False
+        
+        data: bytes = bytes([
+            motorSpeed[deviceEnums.Side.LEFT],
+            motorSpeed[deviceEnums.Side.RIGHT],
+        ])
+        return stmUART.requestActuatorControl(deviceEnums.ActuatorControlType.STS_MOTOR, data)
+        
 
     # def setEncoderValue(self, encoderValue: dict[deviceEnums.Side, int]):
     #     """
@@ -125,9 +216,9 @@ class gyroData:
     @note 単位はdeg
     """
 
-    x: int
-    y: int
-    z: int
+    heading: int
+    pitch: int
+    roll: int
 
 
 class Gyro:
@@ -142,7 +233,7 @@ class Gyro:
         self.data: gyroData = gyroData(0, 0, 0)
         pass
 
-    def setValue(self, gyroData):
+    def setValue(self, gyroData: gyroData):
         self.data = gyroData
 
     def getValue(self) -> gyroData:
@@ -197,8 +288,49 @@ class RescueKitServo:
         @param num: レスキューキットの数
         @param side: レスキューキットを落とすサイド
         """
+        global stmUART
+
+        data: bytes = bytes([
+            side.value,
+            num,
+        ])
+        stmUART.requestActuatorControl(deviceEnums.ActuatorControlType.RESCUE_KIT,
+                                        data)
+        
         pass
 
+class LED:
+    def __init__(
+        self,
+    ):
+        pass
+    
+    def setLEDColor(self, r: int, g: int, b: int)->bool:
+        """
+        @brief LEDの色を設定する
+        @param r: 赤の値(0~255)
+        @param g: 緑の値(0~255)
+        @param b: 青の値(0~255)
+        """
+        global stmUART
+        # 値の範囲チェック
+        if not (0 <= r <= 255):
+            print("invalid LED color value")
+            return False
+        if not (0 <= g <= 255):
+            print("invalid LED color value")
+            return False
+        if not (0 <= b <= 255):
+            print("invalid LED color value")
+            return False
+        
+        data: bytes = bytes([
+            r,
+            g,
+            b,
+        ])
+        return stmUART.requestActuatorControl(deviceEnums.ActuatorControlType.LED, data)
+        
 
 class STM:
     def __init__(
@@ -211,11 +343,28 @@ class STM:
         # self.display: Display = Display()
         self.switch: Switch = Switch()
         self.rescuekitservo: RescueKitServo = RescueKitServo()
+        self.led: LED = LED()
 
-    def update(self):
+    def update(self)->bool:
         global stmUART
-        uartData: UARTData = stmUART.read()
-
-        # 各デバイスのデータを更新してね
-        # ........
-        pass
+        
+        data = stmUART.requestSensorValues()
+        if data is None:
+            return False
+        else:
+            self.unitv.setStatus({
+                deviceEnums.Side.LEFT: deviceEnums.UnitVStatus(data[0]),
+                deviceEnums.Side.RIGHT: deviceEnums.UnitVStatus(data[1]),
+            })
+            self.loadcell.setValue({
+                deviceEnums.Side.LEFT: data[2],
+                deviceEnums.Side.RIGHT: data[3],
+            })
+            # ジャイロの値は360度を1/2に圧縮して送信されるので、2倍にして元に戻す
+            self.gyro.setValue(gyroData(data[4]*2, data[5]*2, data[6]*2))    
+            # data[7]の8bit目がプッシュスイッチ1の値、7bit目がトグルスイッチ1の値
+            self.switch.setValue(
+                pushSwitch1=bool((data[7] >> 7) & 0x01),
+                toggleSwitch1=bool((data[7] >> 6) & 0x01),
+            )
+            return True
