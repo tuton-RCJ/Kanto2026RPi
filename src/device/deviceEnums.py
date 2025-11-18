6c6845f9a16535982d1fa96114bf3f0ff4185020
shuji <shuji.igarashi@gmail.com>
2025-11-18 15:37:20 +0900

    applied python black formatter

--- src/device/deviceEnums.py
+++ src/device/deviceEnums.py
@@ -1,28 +1,28 @@
 from enum import Enum
 
+
 class Side(Enum):
     LEFT = 0
     RIGHT = 1
-    
 
 
 class UnitVStatus(Enum):
-    UNKNOWN=0
-    NOTHING=1
-    
-    
+    UNKNOWN = 0
+    NOTHING = 1
+
+
 class ActuatorControlType(Enum):
-    STS_MOTOR=bytes([1])
-    RESCUE_KIT=bytes([2])
-    LED=bytes([3])
-    BUZZER=bytes([4])
-    
+    STS_MOTOR = bytes([1])
+    RESCUE_KIT = bytes([2])
+    LED = bytes([3])
+    BUZZER = bytes([4])
+
     def dataLength(self):
-        if self==ActuatorControlType.STS_MOTOR:
+        if self == ActuatorControlType.STS_MOTOR:
             return 2
-        elif self==ActuatorControlType.RESCUE_KIT:
+        elif self == ActuatorControlType.RESCUE_KIT:
             return 2
-        elif self==ActuatorControlType.LED:
+        elif self == ActuatorControlType.LED:
             return 3
-        elif self==ActuatorControlType.BUZZER:
+        elif self == ActuatorControlType.BUZZER:
             return 1
