122f7debb1ed25b6d7dedd4b28847295dbe6672d
mitu <mitus16@outlook.jp>
2025-11-22 22:37:33 +0900

    fix wrong indent in main.py

--- src/main.py
+++ src/main.py
@@ -11,37 +11,36 @@ def main():
     mapInstance = mazeMap.mazeMap()
     stmInstance = stm.STM()
     lidarInstance = LiDAR.initializeLidar()
-
-    while True:
-        """
-        points = LiDAR.getLiDARScan(lidarInstance)
-        frontDist = LiDAR.getCertainAngleDist(0, points)
-        print(f"Front Distance: {frontDist} cm")
-        angle = LiDAR.getRelativeAngle(
-            mazeEnums.absDirection.NORTH.value,
-            mapInstance.frontDirection.value,
-            0,
-            20,
-            points,
-        )
-        print(f"Abs Angle on front deg: {min(angle, 360 - angle)} deg")
-        """
-        moveTile.detectWall(lidarInstance, mapInstance)
-        print(mapInstance.wallTypes[20][20])
-        nextDirection = mapInstance.getNearestUnexploredTile()
-        print(f"Next Direction: {nextDirection}")
-        while nextDirection is not None:
-            for direction in nextDirection:
-                moveTile.moveNextTile(direction, mapInstance, stmInstance, lidarInstance)
-            nextDirection = mapInstance.getNearestUnexploredTile()
-
-            returnPath = mapInstance.getPathTo((20, 20))
-            print(f"Return Path: {returnPath}")
-            
-            if returnPath is not None:
-                for direction in returnPath:
-                    moveTile.moveNextTile(direction, mapInstance, stmInstance, lidarInstance)
+    try:
+        while True:
             """
+            points = LiDAR.getLiDARScan(lidarInstance)
+            frontDist = LiDAR.getCertainAngleDist(0, points)
+            print(f"Front Distance: {frontDist} cm")
+            angle = LiDAR.getRelativeAngle(
+                mazeEnums.absDirection.NORTH.value,
+                mapInstance.frontDirection.value,
+                0,
+                20,
+                points,
+            )
+            print(f"Abs Angle on front deg: {min(angle, 360 - angle)} deg")
+            """
+            moveTile.detectWall(lidarInstance, mapInstance)
+            print(mapInstance.wallTypes[20][20])
+            nextDirection = mapInstance.getNearestUnexploredTile()
+            print(f"Next Direction: {nextDirection}")
+            while nextDirection is not None:
+                for direction in nextDirection:
+                    moveTile.moveNextTile(direction, mapInstance, stmInstance, lidarInstance)
+                nextDirection = mapInstance.getNearestUnexploredTile()
+
+                returnPath = mapInstance.getPathTo((20, 20))
+                print(f"Return Path: {returnPath}")
+                
+                if returnPath is not None:
+                    for direction in returnPath:
+                        moveTile.moveNextTile(direction, mapInstance, stmInstance, lidarInstance)
     except:
         import traceback
         traceback.print_exc()
