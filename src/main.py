48c36668e43b9a19bdc414efddfcda49f896b57d
mitu <mitus16@outlook.jp>
2026-01-11 22:04:53 +0900

    fix LoP process in return to start sequence

--- src/main.py
+++ src/main.py
@@ -80,18 +80,51 @@ def main():
             stmInstance.buzzer.playMusic(buzzerSongs.hotaru)       
             returnPath = mapInstance.getPathTo((20, 20))
             print(f"Return Path: {returnPath}")
-            
+
             if returnPath:
                 for direction in returnPath:
                     moveTile.moveNextTile(direction, mapInstance, stmInstance, lidarInstance)
+                    toggleswitchFlag = False
+                    stmInstance.update()
+                    if stmInstance.switch.getToggleSwitch1():
+                        print("Return to start paused. Toggle switch 1 to resume.")
+                    while stmInstance.switch.getToggleSwitch1():
+                        stmInstance.update()
+                        toggleswitchFlag = True       
+
+                    if toggleswitchFlag:
+                        print("Exploration resumed.")
+                        toggleswitchFlag = False
+                        nowAngle = stmInstance.gyro.getValue().heading
+                        nowDirection = None
+                        error = 1e9
+                        for direction in mazeEnums.absDirection:
+                            diff = abs(nowAngle - direction.value)
+                            if diff > 180:
+                                diff = 360 - diff
+                            if diff < error:
+                                error = diff
+                                nowDirection = direction
+                        mapInstance.loadCache(nowDirection=nowDirection)
+                        mapInstance.renderKnownTileAndWall()
+                        time.sleep(1)  # Allow time for stabilization after resuming
+                        stmInstance.update()
+                        moveTile.detectWall(lidarInstance, mapInstance)
+                        tileType = moveTile.detectTileColor()
+                        mapInstance.setTileType(tileType)
+                        moveTile.rescueVictim(mapInstance, stmInstance)
+                        moveTile.flashLED(stmInstance, loopCount=1, intervalSec=0, color=[0,0,0]) 
+                        break
                     print(mapInstance.renderKnownTileAndWall())
-            print("Robot now at the starting position, Congratulations!")
-            stmInstance.buzzer.playMusic(buzzerSongs.matuken) 
-            moveTile.flashLED(stmInstance, loopCount=5, intervalSec=1, color=[255,255,255])  # Flash white LED to indicate completion
-            LiDAR.liDARShutdown(lidarInstance)
-            stmInstance.sts3032.stop()
-            print(mapInstance.renderKnownTileAndWall())
-            exit(0)
+            else:
+                print("Robot now at the starting position, Congratulations!")
+                stmInstance.buzzer.playMusic(buzzerSongs.matuken) 
+                moveTile.flashLED(stmInstance, loopCount=5, intervalSec=1, color=[255,255,255])  # Flash white LED to indicate completion
+                LiDAR.liDARShutdown(lidarInstance)
+                stmInstance.sts3032.stop()
+                print(mapInstance.renderKnownTileAndWall())
+                exit(0)
+            continue
     except:
         import traceback
         traceback.print_exc()
