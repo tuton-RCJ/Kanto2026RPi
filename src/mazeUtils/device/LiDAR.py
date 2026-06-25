07fa97697a6d7fbc4d7c23f70de010254228557e
shuji <shuji.igarashi@gmail.com>
2026-06-25 09:31:14 +0900

    detect obstacles with LiDAR

--- src/mazeUtils/device/LiDAR.py
+++ src/mazeUtils/device/LiDAR.py
@@ -6,6 +6,7 @@ from config import get_logger
 from . import deviceConstraints
 from . import deviceEnums
 from typing import overload
+import math
 
 logger = get_logger(__name__)
 
@@ -146,6 +147,106 @@ def isWallAheadTile(points: list[Point], side: deviceEnums.Side) -> bool:
                 ):
                     return False
     return res
+
+
+def judgeWallCertainAngle(points: list[Point], angle: int) -> int:
+    """
+    @brief 指定した角度について、中央障害物・左障害物・右障害物・壁・壁なしのいずれかを判定する
+    @param points: LiDAR のスキャンデータのリスト
+    @param angle: 判定する角度(度)
+    @return 壁なし: 0, 壁: 1, 中央障害物: 2, 左障害物: 3, 右障害物: 4
+    """
+    center_dist = getCertainAngleDist(angle, points)
+
+    # 連結成分を判定
+    # angleから±45度の範囲を探索し、連結成分の長さを計算する
+    CONNECTED_COMPONENT_THRESHOLD_CM = 4  # 連結成分の閾値(cm)
+    pre_left_point = Point(range=center_dist, angle=angle)
+    pre_right_point = Point(range=center_dist, angle=angle)
+    continue_left = True
+    continue_right = True
+    connected_component_length = 0  # 連結成分の長さ(cm) 曲線の長さ
+    for dp in range(0, 46, 2):
+        left_point = Point(
+            range=getCertainAngleDist(angle - dp, points), angle=angle - dp
+        )
+        right_point = Point(
+            range=getCertainAngleDist(angle + dp, points), angle=angle + dp
+        )
+        if abs(
+            left_point.range - pre_left_point.range
+        ) > CONNECTED_COMPONENT_THRESHOLD_CM or (not continue_left):
+            continue_left = False
+        else:
+            connected_component_length += math.sqrt(
+                pre_left_point.range**2
+                + left_point.range**2
+                - 2
+                * pre_left_point.range
+                * left_point.range
+                * math.cos(math.radians(left_point.angle - pre_left_point.angle))
+            )
+            pre_left_point = left_point
+
+        if abs(
+            right_point.range - pre_right_point.range
+        ) > CONNECTED_COMPONENT_THRESHOLD_CM or (not continue_right):
+            continue_right = False
+        else:
+            connected_component_length += math.sqrt(
+                pre_right_point.range**2
+                + right_point.range**2
+                - 2
+                * pre_right_point.range
+                * right_point.range
+                * math.cos(math.radians(right_point.angle - pre_right_point.angle))
+            )
+            pre_right_point = right_point
+
+    connected_component_length_horizontal = abs(
+        pre_right_point.range * np.sin(np.deg2rad(pre_right_point.angle - angle))
+    ) + abs(pre_left_point.range * np.sin(np.deg2rad(pre_left_point.angle - angle))) # 連結成分の長さ(cm) 見ている方向に垂直な成分を見る
+
+    if (
+        center_dist < deviceConstraints.WALL_DETECTION_THRESHOLD_CM
+        and connected_component_length_horizontal > 20
+    ):
+        return 1  # 壁
+    elif (
+        center_dist < deviceConstraints.WALL_DETECTION_THRESHOLD_CM
+        and connected_component_length_horizontal <= 20
+    ):
+        return 2  # 中央障害物
+
+    obstacle_left_flag = False
+    obstacle_right_flag = False
+    for dp in range(0, 46, 3):
+        left_point = Point(
+            range=getCertainAngleDist(angle - dp, points), angle=angle - dp
+        )
+        right_point = Point(
+            range=getCertainAngleDist(angle + dp, points), angle=angle + dp
+        )
+
+        if left_point.range < 15 and abs(left_point.range * np.sin(np.deg2rad(dp))) < 8:
+            obstacle_left_flag = True
+        if (
+            right_point.range < 15
+            and abs(right_point.range * np.sin(np.deg2rad(dp))) < 8
+        ):
+            obstacle_right_flag = True
+
+    if obstacle_left_flag and not obstacle_right_flag:
+        return 3  # 左障害物
+    elif not obstacle_left_flag and obstacle_right_flag:
+        return 4  # 右障害物
+    elif obstacle_left_flag and obstacle_right_flag:
+        logger.warning(
+            "Both left and right obstacles detected, returning central obstacle"
+        )
+        return 2  # 中央障害物
+    else:
+        return 0  # 壁なし
 
 
 def liDARShutdown(lidar: ydlidar.CYdLidar):
