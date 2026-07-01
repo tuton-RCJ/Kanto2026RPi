792392082de695f493e33c78fc6fd90400cb3594
shuji4649 <shuji.igarashi@gmail.com>
2026-07-02 08:49:06 +0900

    fix victim detection bug!!!

--- src/mazeUtils/moveTile.py
+++ src/mazeUtils/moveTile.py
@@ -1660,7 +1660,7 @@ def moveTile(
                                 [
                                     mazeEnums.absDirection(
                                         (
-                                            mapInstance.frontDirection.value
+                                            direction
                                             + (90 if s == deviceEnums.Side.LEFT else 270)
                                         )
                                         % 360
@@ -1685,7 +1685,7 @@ def moveTile(
                                 [
                                     mazeEnums.absDirection(
                                         (
-                                            mapInstance.frontDirection.value
+                                            direction
                                             + (90 if s == deviceEnums.Side.LEFT else 270)
                                         )
                                         % 360
