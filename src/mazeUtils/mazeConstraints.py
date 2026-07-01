53eb3ab11a7c3035abfdcc2a0fa26e48f6e7683d
shuji4649 <shuji.igarashi@gmail.com>
2026-07-01 16:37:40 +0900

    do not destroy map after wall detection error but update

--- src/mazeUtils/mazeConstraints.py
+++ src/mazeUtils/mazeConstraints.py
@@ -204,7 +204,7 @@ TURN_ANGLE_WHEN_DROP_MULTIPLE_KITS: int = (
 TURN_180_WHEN_LACK_OF_KIT: bool = False  # 救助キットがない場合に180度回転するかどうか
 
 ###### エラーハンドリング
-DESTROY_ALL_INTERNAL_MAP_WHEN_WALL_DETECTION_ERROR: bool = True  # 壁検出エラーが発生した場合に、内部マップを全て破棄するかどうか
+DESTROY_ALL_INTERNAL_MAP_WHEN_WALL_DETECTION_ERROR: bool = False  # 壁検出エラーが発生した場合に、内部マップを全て破棄するかどうか
 DESTROY_ALL_INTERNAL_MAP_WHEN_WALL_DETECTION_ERROR_ONLY_FRONT: bool = True # 壁検出エラーを正面でのみ出すようにする
 
 
