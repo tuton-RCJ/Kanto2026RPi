9e5f2f4fb682c8eb9ea5900a2f72344b0eaa3497
shuji4649 <shuji.igarashi@gmail.com>
2026-07-05 08:54:10 +0900

    to avoid obstacle correction

--- src/mazeUtils/moveTile.py
+++ src/mazeUtils/moveTile.py
@@ -1425,13 +1425,13 @@ def moveTile(
     _judge_front_wall_result = LiDAR.judgeWallCertainAngle(0, pts)
     steer_gain_correction_due_to_obstacle: float = 1.0
     time_correction_due_to_obstacle: float = 0.0
-    if _judge_front_wall_result == deviceEnums.judgeWallResult.LEFT_OBSTACLE:
+    if _judge_front_wall_result == deviceEnums.judgeWallResult.LEFT_OBSTACLE and mazeEnums.USE_OBSTACLE_DETECTION_MODE_WHEN_DETECTING_WALL:
         turnToCertainDirection(
             (mapInstance.frontDirection.value - 25) % 360, stmInstance
         )
         steer_gain_correction_due_to_obstacle = 0.7
         time_correction_due_to_obstacle = 0.1
-    elif _judge_front_wall_result == deviceEnums.judgeWallResult.RIGHT_OBSTACLE:
+    elif _judge_front_wall_result == deviceEnums.judgeWallResult.RIGHT_OBSTACLE and mazeEnums.USE_OBSTACLE_DETECTION_MODE_WHEN_DETECTING_WALL:
         turnToCertainDirection(
             (mapInstance.frontDirection.value + 25) % 360, stmInstance
         )
